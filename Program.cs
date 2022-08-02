68c7f19278aa74b0d39df77236c90bde06438066
mani1 <mani1@NANI143>
2022-08-02 16:21:43 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HornomicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC4_HornomicNumber.CheckUC4_hornomicnumber();
+        }
+    }
