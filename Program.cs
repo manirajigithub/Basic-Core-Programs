f571c01784cbf20d5cb4db046775f8542677594b
mani1 <mani1@NANI143>
2022-08-02 15:51:08 +0530

    Added program .cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC2_LeapYear.CheckUC2_LeapYear();
+        }
+    }
+}
