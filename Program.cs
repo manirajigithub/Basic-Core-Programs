805b79fa30ce092f4b88b61f78925792f09c2cc0
mani1 <mani1@NANI143>
2022-08-02 15:57:52 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PowerOf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC3_PowerOf2.CheckUC3_Powerof2();
+        }
+    }
+}
