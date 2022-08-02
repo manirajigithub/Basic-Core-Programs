e083545abd63728360b5c2027e8f70b0aea1ca87
mani1 <mani1@NANI143>
2022-08-02 22:26:04 +0530

    Added QuotientAnd Remainder program

---
+++ UC6_QuotientRemainder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotientAndRemainder
+{
+    public static class UC6_QuotientAndRemainder
+    {
+        public static void CheckUC6_QuotientAndRemainder()
+        {
+            Console.WriteLine("Write the dividend number");
+
+            int dividendNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Write the divisor number");
+            int divisorNum = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = dividendNum / divisorNum;
+
+            Console.WriteLine("quotient : " + quotient);
+            int remainder = dividendNum % divisorNum;
+
+            Console.WriteLine("remainder : " + remainder);
+        }
+    }
+}
+
