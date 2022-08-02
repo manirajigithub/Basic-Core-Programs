e23dbfd73200a8379a40ce6b6a5bb4348aea9a80
mani1 <mani1@NANI143>
2022-08-02 22:30:51 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC7_SwapTwoNumbers.CheckUC7_SwapTwoNumbers();
+        }
+    }
+}
