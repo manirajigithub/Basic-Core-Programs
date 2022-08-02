aeb5c9a24e44ba999d0cef0722971f0118d02e62
mani1 <mani1@NANI143>
2022-08-02 21:40:23 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactorNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC5_FactorNumber.CheckUC5_factornumber();
+        }
+    }
+}
