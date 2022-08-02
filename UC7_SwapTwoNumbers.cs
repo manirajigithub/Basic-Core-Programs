36e9bfcda85af1eb50ed7b4da3beaaff40fab9ac
mani1 <mani1@NANI143>
2022-08-02 22:34:40 +0530

    Added Swap Two Numbers

---
+++ UC7_SwapTwoNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapTwoNumbers
+{
+    public static class UC7_SwapTwoNumbers
+    {
+        public static void CheckUC7_SwapTwoNumbers()
+        {
+            int firstNum = 100;
+            int secNum = 50;
+
+            firstNum *= secNum;
+            secNum = firstNum / secNum;
+            firstNum /= secNum;
+
+            Console.WriteLine("SwapValue of firstNum :" + firstNum);
+            Console.WriteLine("SwapValue of secNum:" + secNum);
+        }
+    }
+}
+
