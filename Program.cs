b5036cb29f877dab57abfd32caac7b8a7f2dc5fe
mani1 <mani1@NANI143>
2022-08-02 22:17:47 +0530

    Added program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuotientAndRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC6_QuotientAndRemainder.CheckUC6_QuotientAndRemainder();
+        }
+    }
+}
