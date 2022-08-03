298c39c62040c4abbaec9e34b9bd9f8499a657ff
mani1 <mani1@NANI143>
2022-08-03 21:41:48 +0530

    Added Largest Number Program

---
+++ UC10_LargestNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestNumber
+{
+    public static class UC10_LargestNumber
+    {
+        public static void CheckUC10_LargestNumber()
+        {
+            Console.WriteLine("Type Any 3 Numbers");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            if (x > y && x > z)
+            {
+                Console.WriteLine("X is greater");
+            }
+            else if (y > z && y > x)
+            {
+                Console.WriteLine("Y is greater");
+            }
+            else
+            {
+                Console.WriteLine("Z is greater");
+            }
+        }
+    }
+}
+
+           
+
