344b3c1a21ebd105a8d4642d93f204e81750de7e
mani1 <mani1@NANI143>
2022-08-03 21:20:36 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HornomicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC9_VowelOrConsonant.CheckUC9_VowelOrConsonant();
+        }
+    }
+}
