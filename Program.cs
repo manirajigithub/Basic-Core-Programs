c9e80a699c2c8bdca434c115f230a1f70b3b5044
mani1 <mani1@NANI143>
2022-08-03 20:37:32 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC8_EvenOrOdd.CheckUC8_EvenOrOdd();
+        }
+    }
+}
