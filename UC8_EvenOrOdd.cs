d50151fc4306592b9788ece1ec0132ef2288b8c1
mani1 <mani1@NANI143>
2022-08-03 20:45:56 +0530

    Added EvenOrOdd program

---
+++ UC8_EvenOrOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    public static class UC8_EvenOrOdd
+    {
+        public static void CheckUC8_EvenOrOdd()
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int C = A + B;
+            Console.WriteLine(C);
+            if (C % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
