9c15c1bf3f0ee1f9fccac207569f5dfd8b0ea9e5
mani1 <mani1@NANI143>
2022-08-03 21:36:59 +0530

    Added Program .cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC10_LargestNumber.CheckUC10_LargestNumber();
+        }
+    }
+}
+
